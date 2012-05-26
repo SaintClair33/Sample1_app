0063db4dba3d37ea5920bd357d06eb1d4c740b8f
Michael <ceo.wonu@gmail.com>
2012-05-26 14:27:53 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Sample1App::Application.routes.draw do
-  get "pages/home"
+  
+  get "user/new"
 
-  get "pages/contact"
 
-  get "pages/about"
+  match '/signup', :to => 'user#new' 
 
+  match '/contact', :to => 'pages#contact'
+  match '/about',  :to => 'pages#about'
+  match '/help',   :to => 'pages#help'
+  root             :to => 'pages#home'
+  
 end
